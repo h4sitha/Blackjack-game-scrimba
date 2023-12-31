d8c1f9c5c5c8954eaec3e089b49972e115fd58ff
h4sitha <hasithamania123@gmail.com>
2023-12-31 15:40:21 +0530

    Remove the game notes

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// When the start game button pressed 2 random cards will be generated.
-
 let firstCard;
 let secondCard;
 
 let cards;
-
 let cardsSum;
 
 
 let statusPara = document.getElementById("game-status");
 let cardsPara = document.getElementById("cards-recieved");
 let sumPara = document.getElementById("cards-sum");
-
 
 let startButton = document.getElementById("start-btn");
 let newCardButton = document.getElementById("newcard-btn");
@@ -24,14 +20,11 @@ function startGame() {
     firstCard = randomCard();
     secondCard = randomCard();
 
-    cards = [];
+    cards = [firstCard, secondCard];
 
     function randomCard() {
         return (Math.floor(Math.random() * 13) + 1);
     }
-
-    cards.push(firstCard);
-    cards.push(secondCard);
 
     cardsSum = firstCard + secondCard;
     
@@ -59,19 +52,3 @@ function startGame() {
 function getNewCard() {
     console.log("New Card Button");
 }
-
-// If the sum of those 2 cards are less than 21, then the player will be able to request another card.
-
-// If the sum of those 2 cards is equal to 21, he wins.
-
-// If the sum of those 2 cards exceeds 21, then he's out of the game.
-
-// The game stops if the sum reaches 21 or more.
-
-
-// Random cards will be shown in the cards paragraph.
-
-// Sum of the all cards will be shown in the sum paragraph.
-
-
-// Player can request a new card only if the sum is below 21. If the sum is 21 or more, the new button won't work.
