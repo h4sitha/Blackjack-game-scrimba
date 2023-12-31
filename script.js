03e73dadf4b0283db6b076a39074a8c19461b303
h4sitha <hasithamania123@gmail.com>
2023-12-31 13:53:50 +0530

    Update the script file with how the game works

---
+++ script.js
@@ -0,0 +1,17 @@
+// When the start game button pressed 2 random cards will be generated.
+
+// If the sum of those 2 cards are less than 21, then the player will be able to request another card.
+
+// If the sum of those 2 cards is equal to 21, he wins.
+
+// If the sum of those 2 cards exceeds 21, then he's out of the game.
+
+// The game stops if the sum reaches 21 or more.
+
+
+// Random cards will be shown in the cards paragraph.
+
+// Sum of the all cards will be shown in the sum paragraph.
+
+
+// Player can request a new card only if the sum is below 21. If the sum is 21 or more, the new button won't work.
